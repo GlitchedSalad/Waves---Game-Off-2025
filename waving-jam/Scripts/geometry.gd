c4cd4fcb2c08db863ccad3aa79be6469d48f38da
Daniel Shawcroft <67482161+GlitchedSalad@users.noreply.github.com>
2025-11-29 23:52:15 -0700

    Delete waving-jam directory

--- waving-jam/Scripts/geometry.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-
-
-func _on_death_plane_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		body.die()
-	if body.is_in_group("Enemy"):
-		body.hit(100)
--- waving-jam/Scripts/title.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/HostScenes/world.tscn")
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
